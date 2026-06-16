7e0be1c70adaad5ff4a1e5b90f75b43b6d7db6ac
Dhimasekaputraa <dhimasekaputraa@gmail.com>
2026-06-16 15:15:31 +0700

    [MILESTONE 4] now shell can execute external command using fork and creating child process and make the parent process wait

---
+++ executor.py
@@ -0,0 +1,24 @@
+import os
+
+class Executor:
+    @staticmethod
+    def execute_external(command, args):
+        try:
+            pid = os.fork()
+        except OSError as e:
+            print(f"fork failed: {e}")
+            return
+        
+        if pid == 0:
+            try:
+                os.execvp(
+                    command, [command] + args
+                )
+            except FileNotFoundError:
+                print(f"{command}: command not Found")
+            except Exception as e:
+                print(f"execution error: {e}")
+            os._exit(1)
+
+        else:
+            os.waitpid(pid, 0)
