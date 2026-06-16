a6d95c0bff055c26a076b0deb6e73adb8902da59
Dhimasekaputraa <dhimasekaputraa@gmail.com>
2026-06-16 15:13:54 +0700

    moving promptline function from commands.py into new file prompt.py

---
+++ prompt.py
@@ -0,0 +1,41 @@
+import os
+import getpass
+import socket
+
+def get_user_info():
+    try :
+        username = getpass.getuser()
+    except Exception :
+        username = os.environ.get("USER") or os.environ.get("USERNAME") or "USER"
+    
+    hostname = socket.gethostname()
+
+    return {
+        "user": username,
+        "host": hostname
+    }
+
+def format_path(max_depth=3):
+    cwd = os.getcwd()
+    home = os.path.expanduser("~")
+
+    if cwd.startswith(home):
+        cwd = "~" + cwd[len(home):]
+
+    parts = cwd.split("/")
+
+    if len(parts) > max_depth + 1:
+        cwd = f".../{parts[-2]}/{parts[-1]}"
+
+    return cwd
+
+
+def get_prompt():
+    user_info = get_user_info()
+    cwd = format_path()
+
+    GREEN = "\033[32m"
+    BLUE = "\033[34m"
+    RESET = "\033[0m"
+
+    return f"{GREEN}{user_info['user']}{RESET} | {BLUE}{user_info['host']}{RESET} : {cwd}$ "
