cf51c13bba4e70266353bbf63209538d0a33b622
Dhimasekaputraa <dhimasekaputraa@gmail.com>
2026-06-30 13:01:44 +0700

    update the parser for operator in redirection and pipe to make it more general

--- prompt.py
+++ prompt.py
@@ -1,6 +1,9 @@
 import os
 import getpass
 import socket
+import shlex
+from commands import Command
+from executor import Executor
 
 def get_user_info():
     try :
@@ -41,3 +44,66 @@ def get_prompt():
     RESET = "\033[0m"
 
     return f"\n{GREEN}{user_info['user']}{RESET} | {BLUE}{user_info['host']}{RESET} : {cwd}$ "
+
+def parse_and_execute(user_input):
+
+    token = shlex.split(user_input)
+    if not token:
+        return
+    
+    if any(op in token for op in ("|", "<", ">", ">>")):
+        commands = []
+        current = []
+
+        stdin = None
+        stdout = None
+        append = False
+
+        i = 0
+
+        while i < len(token):
+            t = token[i]
+
+            if t == "|":
+                commands.append((current[0].lower(), current[1:]))
+                current = []
+
+            elif t == "<":
+                stdin = token[i + 1]
+                i += 1
+            
+            elif t == ">":
+                stdout = token[i + 1]
+                append = False
+                i += 1
+            
+            elif t == ">>":
+                stdout = token[i + 1]
+                append = True
+                i += 1
+            
+            else:
+                current.append(t)
+
+            i += 1
+
+        if current:
+            commands.append((current[0].lower(), current[1:]))
+        
+        if len(commands) > 1:
+            Executor.execute_pipeline(commands, stdin=stdin, stdout=stdout, append=append)
+        
+        else:
+            command, args = commands[0]
+            command = command.lower()
+
+            Executor.execute_redirection(command, args, stdin=stdin, stdout=stdout,append=append)
+
+        return
+
+    command = token[0].lower()
+    args = token[1:]
+
+    cmd = Command(user_input, token, command, args)
+    cmd.execute_commands()
+    
